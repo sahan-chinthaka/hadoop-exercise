3981e71410f4ecdaa793d59ac5c74789ea2ee278
sahan-chinthaka <sahanchinthaka04@gmail.com>
2024-11-18 21:46:02 +0530

    Add the lab sheet

--- code.bash
+++
@@ -1,8 +0,0 @@
-hdfs dfs -mkdir -p /user/hadoop/input
-hdfs dfs -put input.txt /user/hadoop/input
-
-hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-*.jar \
-    -input /user/hadoop/input/* \
-    -output /user/hadoop/output \
-    -mapper mapper.py \
-    -reducer reducer.py
--- mapper.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-for line in sys.stdin:
-    words = line.strip().split()
-    for word in words:
-        print(f"{word}\t1")
